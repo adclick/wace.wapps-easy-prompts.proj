bf50750fa2b9dc15e15e208d65edc4fe9c275609
Nuno Saraiva <nuno.saraiva@wacestudio.com>
2023-11-19 19:02:58 +0000

    dev

--- src/components/Elements/Suggestion.tsx
+++ src/components/Elements/Suggestion.tsx
@@ -1,5 +1,6 @@
-import { Accordion, AccordionControl, AccordionItem, Box, Button, Collapse, Divider, Group, Paper, Rating, Text } from "@mantine/core";
+import { Accordion, AccordionControl, AccordionItem, Box, Button, Collapse, Divider, Group, Paper, Rating, Stack, Text, rem } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { IconArrowLeft, IconArrowRight, IconCheck } from "@tabler/icons-react";
 
 interface Suggestion {
     name: string
@@ -11,14 +12,24 @@ export function Suggestion({ name }: Suggestion) {
             <AccordionControl px={0}>
                 <Group justify="space-between">
                     <Text fw={600} size="sm">{name}</Text>
-                    <Rating size="xs" readOnly color="blue" value={3} />
                 </Group>
             </AccordionControl>
             <Accordion.Panel px={0}>
-                <Group px={0} py={"xs"} justify="space-between">
-                    <Text size="sm">help</Text>
-                    <Button variant="transparent" size="xs">Apply</Button>
-                </Group>
+                <Stack>
+                    <Text size="xs">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but a
+                    </Text>
+                    <Group px={0} py={"xs"} justify="space-between">
+                        <Rating size="xs" readOnly color="blue" value={3} />
+                        <Button
+                            variant="transparent"
+                            size="xs"
+                            leftSection={<IconCheck style={{ width: rem(14), height: rem(14) }} />}
+                        >
+                            USE
+                        </Button>
+                    </Group>
+                </Stack>
             </Accordion.Panel>
         </AccordionItem>
     )
