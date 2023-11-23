5ab0330806f8dc40577f0b54389263eeb2d17257
Nuno Saraiva <nuno.saraiva@wacestudio.com>
2023-11-23 18:52:28 +0000

    dev

--- src/components/Chat/ChatPanel.tsx
+++ src/components/Chat/ChatPanel.tsx
@@ -25,7 +25,7 @@ export function ChatPanel({
     scrollIntoView
 }: ChatPanel) {
     return (
-        <Stack gap={"md"}>
+        <Stack gap={"md"} my={"xs"}>
             {
                 threads.map((thread: Thread) => {
                     return (
