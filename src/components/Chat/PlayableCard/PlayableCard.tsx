617d3e51d5536848d7f139e6be8449709fcb3972
nsaraiva <nuno.saraiva@wacestudio.com>
2024-01-09 19:51:41 +0000

    dev

---
+++ src/components/Chat/PlayableCard/PlayableCard.tsx
@@ -0,0 +1,83 @@
+import { Avatar, Button, Card, Center, Group, Loader, Stack, Text } from "@mantine/core";
+import { useUser } from "../../../context/UserContext";
+import favicon from "../../../favicon.svg";
+import { PromptRequest } from "../../../model/PromptRequest";
+import { IconPlus } from "@tabler/icons-react";
+import { useDisclosure } from "@mantine/hooks";
+import { ChatSavePromptModal } from "../ChatSavePromptModal/ChatSavePromptModal";
+import { play, textGeneration } from "../../../api/aiApi";
+import { useEffect, useState } from "react";
+import { usePromptsRequests } from "../../../context/PromptsRequestsContext";
+
+interface PlayableCard {
+    promptRequest: PromptRequest,
+}
+
+export function PlayableCard({ promptRequest }: PlayableCard) {
+    const { user } = useUser();
+    const { promptsRequests, setPromptsRequests } = usePromptsRequests();
+    const [response, setResponse] = useState<any>(false);
+    const [newPromptModalOpened, newPromptModaHandle] = useDisclosure(false);
+
+    useEffect(() => {
+        if (response) return;
+        setResponse(<Center><Loader size={"xs"} type="bars" /></Center>);
+        fetch();
+    });
+
+    const fetch = async () => {
+        try {
+            const response = await play(promptRequest.id);
+
+            setResponse(<Text style={{ whiteSpace: "pre-line" }}>{response}</Text>)
+
+            // Update request list
+            const newRequest = PromptRequest.clone(promptRequest);
+            newRequest.response = response;
+            promptsRequests[promptRequest.key] = newRequest;
+            setPromptsRequests(promptsRequests);
+
+        } catch (error) {
+            console.error(error);
+            setResponse(<Text style={{ whiteSpace: "pre-line" }}>"Something went wrong. Please contact support"</Text>)
+        }
+    }
+
+    const save = () => {
+    }
+
+    return (
+        <>
+            <ChatSavePromptModal opened={newPromptModalOpened} handle={newPromptModaHandle} request={promptRequest} />
+            <Card p={"md"} shadow="sm">
+                <Stack gap={"xl"}>
+                    <Stack py={"xs"}>
+                        <Group>
+                            <Avatar src={user.picture} size={"sm"} />
+                            <Text>{user.username}</Text>
+                        </Group>
+                        <Text style={{ whiteSpace: "pre-line" }}>
+                            {promptRequest.title}
+                        </Text>
+                    </Stack>
+                    <Stack>
+                        <Group>
+                            <Avatar variant="white" size={"sm"} src={favicon} alt="no image here" />
+                            <Text>EasyPrompts</Text>
+                        </Group>
+                        {response}
+                        <Group>
+                            <Button
+                                onClick={newPromptModaHandle.open}
+                                variant="subtle"
+                                leftSection={<IconPlus size={14} />}
+                            >
+                                Save
+                            </Button>
+                        </Group>
+                    </Stack>
+                </Stack>
+            </Card>
+        </>
+    )
+}
