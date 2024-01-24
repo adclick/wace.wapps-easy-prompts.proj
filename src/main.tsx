92afed7a7167d6f306f22abf987f0edc966cde19
nsaraiva <nuno.saraiva@wacestudio.com>
2024-01-24 12:05:57 +0000

    dev

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n'
-import '@mantine/core/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <I18nextProvider i18n={i18n}>
