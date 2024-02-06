3b103d485e95b220425e07ff07b0fee06e1d85a8
Nuno Saraiva <ngpsaraiva@gmail.com>
2024-02-06 04:53:10 +0000

    dev

---
+++ src/components/UI/Buttons/IconButton/IconButton.story.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import IconButton from './IconButton';
+import { IconCheck } from '@tabler/icons-react';
+import { Size } from '../../../../utils/uiUtils';
+
+const meta: Meta<typeof IconButton> = {
+    component: IconButton,
+};
+
+export default meta;
+type Story = StoryObj<typeof IconButton>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+ export const Primary: Story = {
+    args: {
+      icon: (<IconCheck />),
+      size: Size.md,
+      variant: "filled"
+    },
+  };
