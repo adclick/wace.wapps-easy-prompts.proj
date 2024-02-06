5cfc1f27fff6c7bf1041cbd2afe4e7d9e4c27776
nsaraiva <nuno.saraiva@wacestudio.com>
2024-02-06 11:53:26 +0000

    fix parameters

--- src/components/Database/Templates/TemplatesList/TemplatesList.tsx
+++ src/components/Database/Templates/TemplatesList/TemplatesList.tsx
@@ -40,7 +40,9 @@ export function TemplatesList({ templatesQuery, databaseListContainerRef }: Temp
 
             if (templates[0].provider) {
                 newUserRequest.provider = Provider.clone(templates[0].provider);
-                newUserRequest.parametersList = ParametersList.buildFromTemplate(templates[0]);
+                // newUserRequest.parametersList = ParametersList.buildFromTemplate(templates[0]);
+                // newUserRequest.parametersList = userPromptRequest.parametersList;
+                // console.log(newUserRequest.parametersList);
             } else {
                 newUserRequest.provider = new Provider();
             }
